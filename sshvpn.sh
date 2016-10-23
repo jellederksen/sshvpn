f371e1a618e11a89ef1c976c4e0d7d5c6343d921
jellederksen <jelle@jellederksen.nl>
2016-10-23 22:05:02 +0200

    Update comments

--- sshvpn.sh
+++ sshvpn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+#
 #Copyright (c) 2016 Jelle Derksen jelle@epsilix.nl
 #Permission is hereby granted, free of charge, to any person obtaining a copy
 #of this software and associated documentation files (the "Software"), to deal
@@ -18,8 +18,8 @@
 #LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
-
-#Sshvpn.sh create an vpn using ssh local forward.
+#
+#sshvpn create a vpn using OpenSSH local forwards.
 
 forward[0]='demo.epsilix.nl,993,wormhole.epsilix.nl,22,jelle'
 
@@ -34,54 +34,67 @@ user_regex='^[a-z_][a-z0-9_]{0,32}$'
 pid_file='/tmp/sshvpn.pid'
 hosts_file='/etc/hosts'
 
-print_stderr() {
+err() {
 	echo "${0##*/}: ${1}" >&2
 }
 
-#Perform basis checks for sshvpn to be able to function.
-do_checks() {
-	if [[ ${UID} != 0 ]]; then
-		print_stderr 'need root privileges'
-	 	return 1
-	fi
-	#check of the calling function equals start_vpn We only
-	#want to check for a pid file when we start sshvpn.
-	if [[ ${FUNCNAME[1]} == start_vpn && -f "${pid_file}" ]]; then
-		print_stderr "found pid file ${pid_file}"
-		return 1
-	fi
-	return 0
+mes() {
+	echo "${0##*/}: ${1}"
 }
 
+#Check if we have root privileges. We need root rights to be able to bind to
+#ports below 1023 on the loopback interface. Without it we won't be able to
+#create a transparent tunnel.If this function is executed while starting
+#sshvpn, we also want to check if a pid file exists. sshvpn should not be
+#able to start while being active.
+do_checks() {
+	if [[ ${UID} != 0 ]]; then
+		err 'need root privileges'
+	 	return 1
+	fi
+	#Check if the calling function is start_vpn and if so check if a pid
+	#file exists.
+	if [[ ${FUNCNAME[1]} == start_vpn && -f "${pid_file}" ]]; then
+		err "pid file ${pid_file} found"
+		return 1
+	fi
+}
+
+
+#Check if the data given is correct and enough to create a OpenSSH forward. The
+#data tested here comes from a element in the forward array. When the tested
+#data is incorrect we return a non-zero exit code.
 check_forward() {
 	#Check if the host to forward to is a valid fqdn or IP-address.
 	if [[ ! $1 =~ $fqdn_regex && ! $1 =~ $ip_regex ]]; then
-		print_stderr "$1 incorrect fqdn or IP-address"
+		err "$1 incorrect fqdn or IP-address"
 		return 1
 	fi
 	#Check if the port to forward to is a valid port number.
 	if [[ $2 -lt 1 || $2 -gt 65535 ]]; then
-		print_stderr "$2 incorrect port number"
+		err "$2 incorrect port number"
 		return 1
 	fi
-	#Check if the host to forward from is a valid fqdn or IP-address.
+	#Check if the host to forward from (ssh stepping stone server) is
+	#a valid fqdn or IP-address.
 	if [[ ! $3 =~ $fqdn_regex && ! $3 =~ $ip_regex ]]; then
-		print_stderr "$3 incorrect fqdn or IP-address"
+		err "$3 incorrect fqdn or IP-address"
 		return 1
 	fi
-	#Check if the port on the host to forward from is a valid port number.
+	#Check if the port on the host to forward from (ssh stepping
+	#stone server) is a valid port number.
 	if [[ $4 -lt 1 || $4 -gt 65535 ]]; then
-		print_stderr "$4 incorrect port number"
+		err "$4 incorrect port number"
 		return 1
 	fi
 	#Check if username is a valid username.
 	if [[ ! $5 =~ $user_regex ]]; then
-		print_stderr "$5 incorrect username"
+		err "$5 incorrect username"
 		return 1
 	fi
-	return 0
 }
 
+#Check if the port we want to listening on with our forward is available.
 check_port() {
 	#Open fd4 as a duplicate of fd2 and redirect stderr to /dev/null.
 	exec 4>&2; exec 2>"${dn}"
@@ -103,41 +116,71 @@ check_port() {
 	fi
 }
 
+#If we have the host IP-address we check if it's in the hosts file. If not we
+#add it to the host file. When we know the hostname we try to resolv it on the
+#SSH stepping stone server and add the hostname and IP-address to the host file.
+#The first octet of the IP-address will be changed to 127 so connections are
+#made to the OpenSSH tunnel on the loopback interface. Adding the hostname to
+#the host file pointing to the loopback interface will create our transparent
+#vpn.
 configure_networking() {
+	#If a IP-address is given and it not in the /etc/hosts file add it.
+	#The IP-address first octet is changed to 127 so we can force traffic
+	#trough the tunnel. We add a comment ${me} after the IP-address so we
+	#can filter out the address later when we want to remove it from the
+	#loopback interface.
 	if [[ ${1} =~ ${ip_regex} ]]; then
 		ip="127.${1#*.}"
 		if ! grep -q "${ip}" "${hosts_file}"; then
 			echo "${ip}     ${ip} #${me}" >> "${hosts_file}"
 		fi
 	else
+		#When a hostname is given we resolv it on the SSH stepping
+		#stone server. The hostname and IP-adress are added to the
+		#/etc/hosts file. We change the first octet of the IP-address
+		#to 127 so we can redirect the traffic trough the tunnel.
 		ip="$(ssh -l "${3}" -i "${ssh_key}" "${2}" "dig ${1} +short" 2> "${dn}")"
-		[[ -z ${ip} ]] && print_stderr "failed to resolv ${1}" && return 1
+		[[ -z ${ip} ]] && err "failed to resolv ${1}" && return 1
 		ip="127.${ip#*.}"
 		if ! grep -q "${1}" "${hosts_file}"; then
 			echo "${ip}     ${1} #${me}" >> "${hosts_file}"
 		fi
 	fi
+	#Check if the given IP-address is already on the loopback
+	#interface if not add it.
 	if ip addr show dev lo | grep "${ip}" > "${dn}" 2>&1; then
+		#Check if the given port is already in use.
 		if check_port "${ip}" "${4}"; then
 			return 0
 		else
-			print_stderr "${ip}:${4} port already in use"
+			err "${ip}:${4} port already in use"
 			return 1
 		fi
 	else
 		if ip addr add "${ip}/32" dev lo; then
 			return 0
 		else
-			print_stderr "failed to add ${ip} to interface lo"
+			err "failed to add ${ip} to interface lo"
 			return 1
 		fi
 	fi
 }
 
+#Create a OpenSSH forward to the SSH stepping stone server
 create_forward() {
+	#${6} Username to login to SSH stepping stone server.
+	#${1} Loopback IP-address.
+	#${3} Port to bind on on the loopback interface IP-address.
+	#${2} IP-address to forward to from SSH stepping stone server,
+	#${3} Port to forward to from the SSH stepping stone server.
+	#${4} hostname or IP-address of the SSH stepping stone server.
+	#${5} OpenSSH port on the SSH stepping stone server.
+	#${dn} redirect errors to /dev/null we dont want to see them.
 	if ssh -fNl "${6}" -i "${ssh_key}" -L "${1}:${3}:${2}:${3}" "${4}" -p "${5}" > "${dn}" 2>&1; then
+		#Determine PID of startend OpenSSH forward and add
+		#it to the pid file in the pid file.
 		if ! lsof -t -i "@${1}:${3}" >> "${pid_file}"; then
-			print_stderr "failed to ssh forward PID to ${pid_file}"
+			err "failed to ssh forward PID to ${pid_file}"
 			return 1
 		else
 			return 0
@@ -149,111 +192,117 @@ create_forward() {
 
 start_vpn() {
 	if ! do_checks; then
-		print_stderr "failed checks"
+		err "failed checks"
 		return 1
 	fi
 	for f in "${forward[@]}"; do
-		#h = hostname, p = port, s = ssh host, x = port, u = username
+		#${h} Host to forward to from the SSH stepping stone server.
+		#${p} = Port on host to forward to from the SSH stepping stone server.
+		#${s} = The SSH stepping stone server.
+		#${x} = The port SSH port on the SSH stepping stone.
+		#${u} = The username on the SSH stepping stone.
 		while IFS=',' read h p s x u; do
 			if ! check_forward "${h}" "${p}" "${s}" "${x}" "${u}"; then
-				print_stderr "forward variable incorrect"
+				err "forward variable incorrect"
 				return 1
 			fi
 			if ! configure_networking "${h}" "${s}" "${u}" "${p}"; then
-				print_stderr "failed to configure networking"
+				err "failed to configure networking"
 				return 1
 			fi
 			if ! create_forward "${ip}" "${h}" "${p}" "${s}" "${x}" "${u}"; then
-				print_stderr "failed to create SSH forward"
+				err "failed to create OpenSSH forward"
 				return 1
 			fi
 		done<<<"${f}"
 	done
-	return 0
 }
 
+#Kill all the OpenSSH forwards and clean up the pid file.
 kill_forward() {
 	while read pid ; do
 		if ! kill ${pid} > "${dn}" 2>&1; then
-			print_stderr "failed to kill ${pid}"
+			err "failed to kill ${pid}"
 			return 1
 		fi
 		done<"${pid_file}"
 		if ! rm "${pid_file}" > "${dn}" 2>&1; then
-			print_stderr "failed to remove ${pid_file}"
+			err "failed to remove ${pid_file}"
 			return 1
 		fi
 	return 0
 }
 
+#Remove all the IP-addressen on the loopback interface we added for the OpenSSH
+#forwards. We can identify the IP-addresses in the /etc/hosts file wearing the
+# comment from the variable ${me}.
 delete_lo_ip() {
+	#Add all sshvpn IP-addresses to the array loopback_ips
 	loopback_ips=( $(grep "${me}" /etc/hosts | cut -d ' ' -f 1) )
+	#Remove the the loopback IP-addresses from the lo interface,
 	for i in "${loopback_ips[@]}"; do
 		if ! ip addr del "${i}/32" dev lo; then
-			print_stderr "failed to remove IP from lo"
+			err "failed to remove IP from lo"
 			return 1
 		fi
 	done
 	return 0
 }
-
+#Check if there are entries in the /etc/hosts file wearing the ${me} tag and
+#if so remove them. We dont want these entries when the tunnels are not up.
 delete_host_resolv() {
-	if grep -q "#${me}" "${hosts_file}" > "${dn}" 2>&1; then
-		if ! sed -i "/#${me}/d" "${hosts_file}"; then
-			print_stderr "failed to remove host from $hosts_file"
-			return 1
-		fi
+	if ! sed -i "/#${me}/d" "${hosts_file}"; then
+		err "failed to remove host from $hosts_file"
+		return 1
 	fi
-	return 0
 }
 
 kill_vpn() {
 	if ! do_checks; then
-		print_stderr "failed checks"
+		err "failed checks"
 		return 1
 	fi
 	if ! kill_forward; then
-		print_stderr "failed to kill ssh forward"
+		err "failed to kill ssh forward"
 		return 1
 	fi
 	if ! delete_lo_ip; then
-		print_stderr "failed to remove IP"
+		err "failed to remove IP"
 		return 1
 	fi
 	if ! delete_host_resolv; then
-		print_stderr "failed to remove host from resolv"
+		err "failed to remove host from resolv"
 		return 1
 	fi
 	return 0
 }
 
-
 main() {
 	if [[ -z ${1} ]]; then
-		print_stderr "${usage}"
-		exit 99
+		err "${usage}"
+		exit 1
 	else
 		while getopts skhv pars; do
 			case "${pars}" in
 			s)
 				if ! start_vpn; then
-					print_stderr "failed to start vpn"
+					err "failed to start vpn"
 					exit 99
 				fi
 				;;
 			k)
 				if ! kill_vpn; then
-					print_stderr "failed to stop vpn"
+					err "failed to stop vpn"
 					exit 99
 				fi
 				exit 0
 				;;
 			h)
-				print_stdout "${usage}"
+				mes "${usage}"
 				exit 0
 				;;
 			*)
-				print_stderr "${usage}"
+				err "${usage}"
 				exit 99
 				;;
 			esac
